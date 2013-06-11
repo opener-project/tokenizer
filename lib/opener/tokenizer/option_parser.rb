5f4d01e8240ce859a896d5c3ed79d1f9280f5e3b
Giannis Melidis <giannismelidis@olery.com>
2013-06-11 12:46:49 +0200

    Changes to option parser

--- lib/opener/tokenizer/option_parser.rb
+++ lib/opener/tokenizer/option_parser.rb
@@ -8,34 +8,26 @@ module Opener
         options = {}
         ::OptionParser.new do |opts|
           opts.banner = "\nUsage: cat some_text.txt | tokenizer [options]\n\n"
-
-          opts.on("-k", "--kaf", "Input is a KAF file with the xml:lang attribute set and the raw text.") do |v|
-            options[:kaf] = v
-          end
           
           opts.on("-l", "--language [LANG]", "Inputs language attribute as an argument.") do |v|
             check_language!(v)
             options[:language] = v
           end
 
-          opts.separator "\n"
-          opts.separator <<-EOF.strip
+          opts.on("-k", "--kaf", "Input is a KAF file with the xml:lang attribute set and the raw text.") do
+            options[:kaf] = true
+          end
+          
 
-      Languages that can be tokenized:
 
-        english (en), french (fr), spanish (es), italian (it) german (de) and dutch (nl)
-
-        If you give a KAF file as an input (-k or --kaf) the language is taken from the xml:lang
-        attribute inside the file. Else it expects that you give the language as an argument (-l or --language)
-
-          EOF
+          opts.separator ""
 
         end.parse!(options_array)
         return options
       end
       
       def self.check_language!(language)
-        raise STDERR.puts "'#{language}' language cannot be tokenized" if !language_list.include?(language.downcase)
+        abort "'#{language}' language cannot be tokenized." if !language_list.include?(language.downcase)
       end
     
       def self.language_list
